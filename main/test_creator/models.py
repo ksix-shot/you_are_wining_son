c104c4e4b2860a6b2595882f20cc53f27a1cc70c
VZSultanov <sltnvv@gmail.com>
2021-12-26 11:59:13 +0300

    refactor model

--- main/test_creator/models.py
+++ main/test_creator/models.py
@@ -1,13 +1,17 @@
 from django.db import models
 import uuid
+from django.utils import timezone
 
 
 class Test(models.Model):
     id = uuid.uuid4()
     name = models.CharField(max_length=512, default=str(id))
+    date_creation = models.DateTimeField(default=timezone.now)
+    date_last_change = models.DateTimeField(default=timezone.now)
 
 
 class Choises(models.Model):
     text = models.CharField(max_length=512)
-    photo = models.TextField()
+    photo = models.TextField(blank=True, null=True)
+    youtube_url = models.CharField(max_length=512, default=None, blank=True, null=True)
     test = models.ForeignKey(Test, on_delete=models.CASCADE)
